041610fd7914f56e5603aefbd4df727ca235caa1
Lionel Paul <43421734+liolabs1973@users.noreply.github.com>
2022-09-11 17:59:53 +0200

    Chap 2 - Draw Shapes
    How to Draw some shape to gamescreen

---
+++ Chap 2 - Draw Shapes/main.lua
@@ -0,0 +1,28 @@
+-- Duck Hunt Challenge By Liolabs Vers 1.0
+--     *******************************************
+--     Création d'un clone du jeu Duck Hunt
+--     sorti sur Nintendo NES
+--     Sons et Spritesheet obtenus sur Spriters Resources
+
+-- Variables et tableaux
+
+-- fonctions
+
+-- Squelette génénal du jeu
+
+
+function love.load()
+   
+end
+
+
+function love.update(dt)
+    
+end
+
+
+function love.draw()
+   love.graphics.rectangle("fill",200,300,400,200)
+   love.graphics.circle("fill", 81,81,80)
+end
+
